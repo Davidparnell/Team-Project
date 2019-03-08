0b22339d4553d1c24a8845c657eadaf7d21c0913
Davidparnell <c15704085@mydit.ie>
2019-03-08 15:02:59 +0000

    XML Fixes and error cleanup

---
+++ MoneyApp/MoneyApp/app/src/main/java/com/moneyapp/EditWallet.java
@@ -0,0 +1,14 @@
+package com.moneyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditWallet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_wallet);
+    }
+}
---
+++ MoneyApp/MoneyApp/app/src/main/java/com/moneyapp/Wallet.java
@@ -0,0 +1,84 @@
+package com.moneyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class Wallet extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton five, ten, twenty, fifty, confirm, wallet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+
+        five = findViewById(R.id.euro5);
+        five.setOnClickListener(this);
+
+        ten = findViewById(R.id.euro10);
+        ten.setOnClickListener(this);
+
+        twenty = findViewById(R.id.euro20);
+        twenty.setOnClickListener(this);
+
+        fifty = findViewById(R.id.euro50);
+        fifty.setOnClickListener(this);
+
+        confirm = findViewById(R.id.Confirm);
+        confirm.setOnClickListener(this);
+
+        wallet = findViewById(R.id.wallet);
+        wallet.setOnClickListener(this);
+    }
+
+    @Override
+    //Functions after a button is pressed
+    public void onClick(View v) {
+        if (v == (View) five)
+        {
+            //Add 5 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 5 added ---",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        else if (v == (View) ten)
+        {
+            //Add 10 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 10 added ---",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        else if (v == (View) twenty)
+        {
+            //Add 20 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 20 added ---",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        else if (v == (View) fifty)
+        {
+            //Add 50 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 50 added ---",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        else if (v == (View) wallet)
+        {
+            //Open EditWallet Activity
+            Intent intent = new Intent(getApplicationContext(), EditWallet.class);
+            startActivity(intent);
+        }
+
+        else if (v == (View) confirm)
+        {
+            //Upon Confirmation Return to hom
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}

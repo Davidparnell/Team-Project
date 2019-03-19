c35ada084bcd14a28e6c14ab02876d8ee58ffafe
Davidparnell <c15704085@mydit.ie>
2019-03-19 12:50:12 +0000

    File Clean up and changing XML basis
    Deleted a sub folder of redundant data, researched methods to change XML files in the java class and began basis

--- MoneyApp/app/src/main/java/com/moneyapp/Wallet.java
+++ MoneyApp/app/src/main/java/com/moneyapp/Wallet.java
@@ -49,68 +49,61 @@ public class Wallet extends AppCompatActivity implements View.OnClickListener {
     @Override
     //Functions after a button is pressed
     public void onClick(View v) {
-        MoneyButtons(v);
-        SwitchDenominations(v);
+        //Money Buttons
+        if (v == (View) five) {
+            //Add 5 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 5 added ---",
+                    Toast.LENGTH_SHORT).show();
+        } else if (v == (View) ten) {
+            //Add 10 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 10 added ---",
+                    Toast.LENGTH_SHORT).show();
+        } else if (v == (View) twenty) {
+            //Add 20 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 20 added ---",
+                    Toast.LENGTH_SHORT).show();
+        } else if (v == (View) fifty) {
+            //Add 50 to wallet, for now toast
+            Toast.makeText(getApplicationContext(), "--- 50 added ---",
+                    Toast.LENGTH_SHORT).show();
+        } else if (v == (View) wallet) {
+            //Open EditWallet Activity
+            Intent intent = new Intent(getApplicationContext(), EditWallet.class);
+            startActivity(intent);
+        } else if (v == (View) confirm) {
+            //Upon Confirmation Return to hom
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+        else if (v == (View) NoteCoin)
+        {
+            SwitchDenominations(v);
+        }
     }
 
     public void SwitchDenominations(View v)
     {
-        String status;
+        Boolean status = (NoteCoin.isChecked());
 
-        if(NoteCoin.isChecked())
-        {
-            //Notes
-        }
-
-        else
+        //checked
+        if(status = true)
         {
             //Coins
+            setContentView(R.layout.activity_wallet_coins);
+
+        }
+
+        //Not Checked
+        else
+        {
+            //Notes
+            setContentView(R.layout.activity_wallet);
         }
     }
 
     public void MoneyButtons(View v)
     {
-        if (v == (View) five)
-        {
-            //Add 5 to wallet, for now toast
-            Toast.makeText(getApplicationContext(), "--- 5 added ---",
-                    Toast.LENGTH_SHORT).show();
-        }
 
-        else if (v == (View) ten)
-        {
-            //Add 10 to wallet, for now toast
-            Toast.makeText(getApplicationContext(), "--- 10 added ---",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        else if (v == (View) twenty)
-        {
-            //Add 20 to wallet, for now toast
-            Toast.makeText(getApplicationContext(), "--- 20 added ---",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        else if (v == (View) fifty)
-        {
-            //Add 50 to wallet, for now toast
-            Toast.makeText(getApplicationContext(), "--- 50 added ---",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        else if (v == (View) wallet)
-        {
-            //Open EditWallet Activity
-            Intent intent = new Intent(getApplicationContext(), EditWallet.class);
-            startActivity(intent);
-        }
-
-        else if (v == (View) confirm)
-        {
-            //Upon Confirmation Return to hom
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        }
     }
 
 

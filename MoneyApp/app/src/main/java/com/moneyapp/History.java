2d43cf8c7f6b64fb7fa3eb3258ee0f4b42cf38be
Mateusz Harcej <c16344691@mydit.ie>
2019-04-03 13:05:36 +0100

    History implemented

--- MoneyApp/app/src/main/java/com/moneyapp/History.java
+++ MoneyApp/app/src/main/java/com/moneyapp/History.java
@@ -4,19 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 import com.moneyapp.Database.AppDatabase;
 import com.moneyapp.Database.WalletDAO;
 import com.moneyapp.Database.WalletData;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class History extends AppCompatActivity {
+public class History extends AppCompatActivity
+{
+    private ListView listView;
+    private List<HistoryData> historyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        listView = findViewById(R.id.historyList);
+        historyList = new ArrayList<>();
 
         //DB object
         AppDatabase database = AppDatabase.getDatabase(getApplicationContext());
@@ -26,15 +34,33 @@ public class History extends AppCompatActivity {
         List<WalletData> walletHistory = walletDAO.getWalletHistory();
 
         //test output to log, can be deleted and formatted later
-        for(int i = 0; i < walletHistory.size(); i++){
+        for(int i = 0; i < walletHistory.size(); i++)
+        {
+            HistoryData item = new HistoryData();
+            item.setDate(walletHistory.get(i).getDate());
+            item.setBalance(String.format("%.02f", walletHistory.get(i).getBalance()));
+            item.setRegister(String.format("%.02f", walletHistory.get(i).getRegister()));
+
             String type;
-            if(walletHistory.get(i).getRegister() == 0){
+            if(walletHistory.get(i).getRegister() == 0)
+            {
                 type = "wallet change";
+                item.setType(type);
             }
-            else{
+            else
+            {
                 type = "transaction";
+                item.setType(type);
             }
+
+            historyList.add(item);
+
             Log.d("HIS", walletHistory.get(i).getDate()+" "+String.format("%.02f", walletHistory.get(i).getBalance())+" "+String.format("%.02f", walletHistory.get(i).getRegister())+" "+type);
         }
+
+        HistoryAdapter adapter = new HistoryAdapter(historyList, getApplicationContext());
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
     }
 }
---
+++ MoneyApp/app/src/main/java/com/moneyapp/HistoryAdapter.java
@@ -0,0 +1,50 @@
+package com.moneyapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class HistoryAdapter extends ArrayAdapter<HistoryData>
+{
+    private List<HistoryData> historyList;
+    private Context context;
+
+    public HistoryAdapter(List<HistoryData> historyList, Context context)
+    {
+        super(context, R.layout.list_items, historyList);
+        this.historyList = historyList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View listViewItem = inflater.inflate(R.layout.list_items, null, true);
+
+        //Text view components
+        TextView textDate = listViewItem.findViewById(R.id.DateView);
+        TextView textBalance = listViewItem.findViewById(R.id.BalanceView);
+        TextView textRegister = listViewItem.findViewById(R.id.RegisterView);
+        TextView textType = listViewItem.findViewById(R.id.TypeView);
+
+        HistoryData items = historyList.get(position);
+
+        textDate.setText(items.getDate());
+        textBalance.setText(items.getBalance());
+        textRegister.setText(items.getRegister());
+        textType.setText(items.getType());
+
+        return listViewItem;
+    }
+}
